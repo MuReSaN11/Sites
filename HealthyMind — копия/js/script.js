f1b1dd451a86a06b79e1c1b6e6a659df07d6158b
MuReSaN11 <99989991+MuReSaN11@users.noreply.github.com>
2022-10-07 10:07:39 +0300

    Set

---
+++ HealthyMind — копия/js/script.js
@@ -0,0 +1,7 @@
+// !трекер
+const tracker = document.querySelector(".tracker");
+document.body.addEventListener("mousemove", e => {
+    tracker.style.left = `${e.clientX}px`;
+    tracker.style.top = `${e.clientY}px`;
+});
+// !Трекер
